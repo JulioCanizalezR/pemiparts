a0bff71da244938de515456d02f9f26511ea6c10
Your Name <you@example.com>
2024-09-24 12:49:58 -0600

    data y handler de registro de inicios

---
+++ api/models/data/registro_inicio.php
@@ -0,0 +1,38 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/registro_inicios_handler.php');
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla CATEGORIA.
+ */
+class registro_inicios_data extends Registros_inicios_handler
+{
+    /*
+     *  Atributos adicionales.
+     */
+    private $data_error = null;
+
+    /*
+     *  Métodos para validar y establecer los datos.
+     */
+
+     
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            $this->data_error = 'El identificador de la entidad es incorrecto';
+            return false;
+        }
+
+ 
+    public function getDataError()
+    {
+        return $this->data_error;
+    }
+
+    
+}
---
+++ api/models/handler/registro_inicios_handler.php
@@ -0,0 +1,41 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once('../../helpers/database.php');
+/*
+ *  Clase para manejar el comportamiento de los datos de la tabla CATEGORIA.
+ */
+class Registros_inicios_handler
+{
+    /*
+     *  Declaración de atributos para el manejo de datos.
+     */
+ 
+     protected $id_registro = null;
+     protected $correo_electronico = null;
+     protected $fecha_hora = null;
+    // Constante para establecer la ruta de las imágenes.
+ 
+    /*
+     *  Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
+     */
+    public function searchRows()
+    {
+        $value = '%' . Validator::getSearchValue() . '%';
+        $sql = 'SELECT id_registro,correo_electronico,fecha_hora
+                FROM tb_registro_inicios
+                WHERE correo_electronico LIKE ?';
+        $params = array($value);
+        return Database::getRows($sql, $params);
+    }
+ 
+    public function readAll()
+    {
+        $sql = 'SELECT ri.id_registro, u.correo_electronico, ri.fecha_hora
+                    FROM tb_registro_inicios ri
+                    INNER JOIN tb_usuarios u ON ri.id_usuario = u.id_usuario';
+            return Database::getRows($sql);
+    }
+
+    
+}
+ 
